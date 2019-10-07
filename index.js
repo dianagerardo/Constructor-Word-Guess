b5c1d528fac03e1895a74cfdd5cd5b8871462e24
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-10-07 16:20:20 -0700

    Adding inquirer function to guess letters

--- index.js
+++ index.js
@@ -8,6 +8,43 @@ console.log(randomWords({exactly: 5, minLength: 4, maxLength: 6}));
 //   * Randomly selects a word and uses the `Word` constructor to store it
 
 //   * Prompts the user for each guess and keeps track of the user's remaining guesses
+let count = 0;
+let playerArray = [];
+var askPlayer = function () {
+
+    if (count < 5) {
+        // runs inquirer and asks the user a series of questions whose replies are
+        // stored within the variable answers inside of the .then statement
+        inquirer.prompt([
+            {
+                name: "guess",
+                message: "Guess a letter!"
+            }
+              
+        ]).then(function (answers) {
+            // initializes the variable newProgrammer to be a programmer object which will take
+            // in all of the user's answers to the questions above
+            let newGuess = answers.guess
+            // printInfo method is run to show that the newProgrammer object was successfully created and filled
+            
+            playerArray.push(newGuess);
+            // add one to count to increment our recursive loop by one
+            count++;
+            // run the askquestion function again so as to either end the loop or ask the questions again
+            askPlayer();
+            console.log(playerArray);
+            console.log("---------------------------------------")
+        });
+        // else statement which prints "all questions asked" to the console
+        // when the code has been run five times
+    }
+    else {
+        console.log("You ran out of guesses!");
+    }
+};
+
+// call askPlayer to run our code
+askPlayer();
 
 // 3. `Letter.js` *should not* `require` any other files.
 
